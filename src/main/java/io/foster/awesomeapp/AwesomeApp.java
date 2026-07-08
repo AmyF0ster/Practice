0735c7ef6a2777f0afd0b21a87a67b28d34d85a4
Amy <iomi030206@gmail.com>
2026-07-09 00:46:17 +0300

    renamed packages

--- src/main/java/io/foster/awesomeapp/AwesomeApp.java
+++ src/main/java/io/foster/awesomeapp/AwesomeApp.java
@@ -1,4 +1,4 @@
-package io.foster.practice;
+package io.foster.awesomeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/io/foster/awesomeapp/AwesomeAppTest.java
+++ src/test/java/io/foster/awesomeapp/AwesomeAppTest.java
@@ -1,4 +1,4 @@
-package io.foster.practice;
+package io.foster.awesomeapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

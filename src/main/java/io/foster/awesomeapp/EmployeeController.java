25c95574b24c30c2aaf4d910c47fc657573dac64
Amy <iomi030206@gmail.com>
2026-07-10 00:42:14 +0300

    changed classes

--- src/main/java/io/foster/awesomeapp/EmployeeController.java
+++ src/main/java/io/foster/awesomeapp/EmployeeController.java
@@ -1,12 +1,18 @@
 package io.foster.awesomeapp;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping ("/employees")
 public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
+    @GetMapping("/employees")
+    public List<Employee> getEmployees(){
+        return employeeService.getAllEmployees();
+    }
 }
--- src/main/java/io/foster/awesomeapp/EmployeeService.java
+++ src/main/java/io/foster/awesomeapp/EmployeeService.java
@@ -3,8 +3,13 @@ package io.foster.awesomeapp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class EmployeeService {
     @Autowired
     private EmployeeRepository employeeRepository;
+    public List<Employee> getAllEmployees(){
+        return (List<Employee>) employeeRepository.findAll();
+    }
 }

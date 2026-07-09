a0711720b2044123a1bc6e6592a3b281d31689e9
Amy <iomi030206@gmail.com>
2026-07-09 16:25:11 +0300

    made data base work

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,28 @@
-INSERT INTO countries (id, name) VALUES (1, 'USA');
-INSERT INTO countries (id, name) VALUES (2, 'France');
-INSERT INTO countries (id, name) VALUES (3, 'Brazil');
-INSERT INTO countries (id, name) VALUES (4, 'Italy');
-INSERT INTO countries (id, name) VALUES (5, 'Canada');
+CREATE TABLE IF NOT EXISTS employee
+(
+    employee_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    position VARCHAR(150) NOT NULL,
+    department VARCHAR(100) NOT NULL,
+    hiring_date DATE NOT NULL
+    );
+
+
+CREATE TABLE IF NOT EXISTS day_type
+(
+    day_type_id INT AUTO_INCREMENT PRIMARY KEY,
+    day_type VARCHAR(50) NOT NULL UNIQUE
+    );
+
+
+CREATE TABLE IF NOT EXISTS timesheet
+(
+    record_id INT AUTO_INCREMENT PRIMARY KEY,
+    date DATE NOT NULL,
+    employee_FK INT NOT NULL,
+    hours FLOAT NOT NULL DEFAULT 0,
+    day_type_FK INT NOT NULL,
+
+    FOREIGN KEY (employee_FK) REFERENCES employee(employee_id) ON DELETE CASCADE,
+    FOREIGN KEY (day_type_FK) REFERENCES day_type(day_type_id) ON DELETE NO ACTION
+    );

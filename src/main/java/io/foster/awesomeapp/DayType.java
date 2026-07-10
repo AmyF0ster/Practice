514fa2382be0e2ead2fe3d6bf0d339f100f910cf
Amy <iomi030206@gmail.com>
2026-07-10 15:25:55 +0300

    changed column names and changed DayType class to Enum

--- src/main/java/io/foster/awesomeapp/DayType.java
+++ src/main/java/io/foster/awesomeapp/DayType.java
@@ -1,12 +1,14 @@
 package io.foster.awesomeapp;
 
 import lombok.Getter;
+import org.springframework.data.annotation.Id;
 
 @Getter
 public enum DayType {
     WORKDAY(1, "Работает"),
     VACATION(2, "В отпуске"),
     DAYOFF(3, "Отгул");
+    @Id
     private Integer dayTypeId;
     private String description;
 
